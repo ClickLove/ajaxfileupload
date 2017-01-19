a9670384f0868c7f11730915fc1eae175163f0dc
wshining <1908699044@qq.com>
2017-01-19 15:38:26 +0800

    add ajaxfileuploadjs

---
+++ ajaxfileupload.js
@@ -0,0 +1,47 @@
+[33mcommit 3a9ab0af1c31f3153b988cbfb7263d77fc6efc32[m
+Author: wshining <1908699044@qq.com>
+Date:   Thu Jan 19 15:23:54 2017 +0800
+
+    add ajaxfileupload by js
+
+[33mcommit ea9679807470aebfd3163c141b74eb6430970bac[m
+Author: wshining <1908699044@qq.com>
+Date:   Fri Dec 30 17:31:50 2016 +0800
+
+    remove test.txt
+
+[33mcommit 21107a3c6d7789b58bc31e792049da9a32d820b6[m
+Author: wshining <1908699044@qq.com>
+Date:   Fri Dec 30 17:29:40 2016 +0800
+
+    add test
+
+[33mcommit 2e160290a9faf80f81682eb7a33f1526a87d1264[m
+Author: wshining <1908699044@qq.com>
+Date:   Fri Dec 30 17:23:10 2016 +0800
+
+    git tracks change
+
+[33mcommit 5c2c2675edafe1ccfc0cace811e9273506f01f08[m
+Author: wshining <1908699044@qq.com>
+Date:   Fri Dec 30 17:19:02 2016 +0800
+
+    understand how stage works
+
+[33mcommit 34beb0da646230d460d0535fb5cece5f7422e871[m
+Author: wshining <1908699044@qq.com>
+Date:   Fri Dec 30 15:36:35 2016 +0800
+
+    append GPL
+
+[33mcommit beb8618cd1b283b7ab3ff0444dda0614b1208f1d[m
+Author: wshining <1908699044@qq.com>
+Date:   Fri Dec 30 15:34:17 2016 +0800
+
+    add distributed 2016.12.30
+
+[33mcommit ea37c2c676b864062e5f6d4cad44eb42349c4141[m
+Author: wshining <1908699044@qq.com>
+Date:   Fri Dec 30 15:31:05 2016 +0800
+
+    wrote a readme.txt
